1b55042898fbc804cc06cfcda4e775198a004e77
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-06-26 04:29:47 +0500

    script

---
+++ script.js
@@ -0,0 +1,82 @@
+
+// masala 1
+
+//   {
+//     let numbers = [-5,3,5,-2]
+
+//     let result = []
+//     numbers.forEach((son) => {
+//         if(son > 0 ){
+//             result.push(son)
+//         }
+//     } )
+//     console.log(result);
+//   }  
+
+
+// masala 2
+
+// const reverseString = (word) => {
+//     return word
+//                 .split("")
+//                 .reverse()
+//                 .join("")
+
+// }
+
+// console.log( reverseString("salom") )
+
+
+
+
+
+    // masala 3
+
+
+
+    // masala 5
+
+    // {
+    //     let numbers = [1,2,3,4,5]
+
+    //     let result = numbers.filter((laylo) => laylo % 2 == 0 )
+    //                         .reduce((sum,laylo) => sum + laylo, 0)
+    //     console.log (result);
+    // }
+    
+
+    // masala 6
+
+ 
+
+
+    
+
+    // masala 9
+    
+
+    
+
+
+
+    // masala 10
+    // console.log( "salom ".repeat(3) );
+
+
+    // masala 11
+
+    // {
+    //     let numbers = [1,2,3] 
+    //     numbers.reverse()
+    //     console.log (numbers);
+    // }
+
+
+    // masala 12
+    // {
+    //     let numbers = [2,1,13,4]
+    //     numbers.sort((a,b) => a-b)
+    //     console.log(numbers);
+    // }
+
+
